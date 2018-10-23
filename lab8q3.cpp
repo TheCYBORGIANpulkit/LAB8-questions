782ed7587528eda032d88a696d1665b75482e6a7
PULKIT OJHA <42231120+TheCYBORGIANpulkit@users.noreply.github.com>
2018-10-23 19:56:02 +0530

    Add files via upload

---
+++ lab8q3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//defining the function
+//defining array
+int klarge (int array[], int n, int k)
+	{
+	
+
+}
+int main(){
+	int n;
+	cout<< "How many elements do you want to take in the array?"<<endl;
+	cin>>n;
+	int arr[n-1];
+	cout<<"Enter the +ve integers you want to take in the array:"<<endl;
+		for(int i=0; i< n; i++)
+		{
+			cin>> arr[i];
+		}
+}
